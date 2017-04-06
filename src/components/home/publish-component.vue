7ffcce27fa30e8a1636649e0abfdf3a7d5406240
KKKyrie <kyrieliu02@163.com>
2017-04-06 10:04:08 +0800

    change layout to flexbox

--- src/components/home/publish-component.vue
+++ src/components/home/publish-component.vue
@@ -1,27 +1,45 @@
 <template>
 	<div>
 		<div class="publish-container">
-			<select class="form-control inline-element info-type" v-model="infoType">
-				<option value="0">我有这本书</option>
-				<option value="1">我要这本书</option>
-			</select>
+			<!-- row 1 -->
+			<div class="row-1">
+				<span>
+					<label for="infoType">类型：</label>
+					<select name="infoType" class="form-control inline-element info-type" v-model="infoType">
+						<option value="0">我有这本书</option>
+						<option value="1">我要这本书</option>
+					</select>
+				</span>
 			
-			<span><label>书名：</label><input class="form-control inline-element book-name" type="text" v-model="bookName" placeholder="无需添加书名号" />
-</span>
-			<span><label>分类：</label><input class="form-control inline-element tags" type="text" v-model="tags" placeholder="至少添加一个，用分号隔开" />
-</span>
-			<br/>
+				<span><label for="bookName">书名：</label><input class="form-control inline-element book-name" type="text" v-model="bookName" placeholder="无需添加书名号" /></span>
+				<span><label for="tags">分类：</label><input class="form-control inline-element tags" type="text" v-model="tags" placeholder="至少添加一个，用分号隔开" /></span>
+			</div>
+			
+			<!-- row 2 -->
+			<div class="row-2">
+				<span>
+					<label for="message">留言：</label>
+					<input name="message" type="text" class="form-control description" v-model="message" placeholder="谈谈你对这本书的感受 / 你为什么想要这本书？">
+				</span>
+			</div>
+			
+			<!-- row 3 -->
+			<div class="row-3">
+				<span>
+					<label for="">共享方式：</label>
+					<select class="form-control inline-element share-type" v-model="shareType">
+						<option value="0">我要交换</option>
+						<option value="1">我要交易</option>
+					</select>	
+				</span>
+				<span v-if="shareType == 1"><label for="">期望价格：</label><input type="text" class="form-control inline-element price" placeholder="0" v-model="price">&nbsp;元</span>
+			</div>
 
-			<input type="text" class="form-control description" v-model="description" placeholder="谈谈你对这本书的感受 / 你为什么想要这本书？">
-
-			<label for="">共享方式：</label>
-			<select class="form-control inline-element share-type" v-model="shareType">
-				<option value="0">我要交换</option>
-				<option value="1">我要交易</option>
-			</select>
-			<span v-if="shareType == 1"><label for="">期望价格：</label><input type="text" class="form-control inline-element price" placeholder="0" v-model="price">元</span>
-			<br />
-			<button class="btn btn-success publish-btn">发布</button>
+			<div class="row-4">
+				<button class="btn btn-success">发布</button>
+				<span>&nbsp;&nbsp;&nbsp;</span>
+				<button class="btn btn-default">取消</button>
+			</div>
 		</div>
 
 	</div>
@@ -34,7 +52,7 @@
 				infoType: 0,
 				bookName: '',
 				tags: '',
-				description: '',
+				message: '',
 				shareType: 1,
 				price: ''
 			}
@@ -49,6 +67,27 @@
 		width:60%;
 		margin: auto;
 		padding: 10px 20px;
+		position: relative;
+	}
+
+	.row-1{
+		display: flex;
+		-webkit-display: flex;
+		justify-content: space-around;
+	}
+
+
+
+	.row-3{
+		display: flex;
+		-webkit-display: flex;
+		justify-content: flex-start;
+	}
+
+	.row-4{
+		display: flex;
+		-webkit-display: flex;
+		justify-content: center;
 	}
 
 	.inline-element{
@@ -83,9 +122,6 @@
 		width: 100px;
 	}
 
-	.publish-btn{
-		margin-left: 90%;
-	}
 
 
 
