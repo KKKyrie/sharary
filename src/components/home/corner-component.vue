bf351ae3c3c82ef83d86185a0953534f09a4df61
KKKyrie <kyrieliu02@163.com>
2017-04-07 19:21:38 +0800

    add navbar & modify the corner label

--- src/components/home/corner-component.vue
+++ src/components/home/corner-component.vue
@@ -10,11 +10,15 @@
 	.corner-container{
 		position: absolute;
 		top: 5px;
-		right: 0;
+		right: -30px;
+		width: 100px;
 		padding: 3px;
 		transform: rotate(45deg);
 		background-color: #42b983;
 		/* #ae2e2e */
 		color: white;
+		text-align: center;
+
+		box-shadow: 0px 5px 5px #ccc;
 	}
 </style>
